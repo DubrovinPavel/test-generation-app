13e420dad609283e55abec60e731b2981996df6a
DubrovinPavel <emberplugin@mail.ru>
2017-12-02 15:11:27 +0000

    Flexberry code generation

---
+++ app/locales/en/models/i-i-s-disney-ticket.js
@@ -0,0 +1,26 @@
+export default {
+  projections: {
+    TicketE: {
+      number: {
+        __caption__: 'number'
+      },
+      person: {
+        __caption__: 'person',
+        name: {
+          __caption__: 'name'
+        }
+      }
+    },
+    TicketL: {
+      number: {
+        __caption__: 'number'
+      },
+      person: {
+        __caption__: 'person',
+        name: {
+          __caption__: 'name'
+        }
+      }
+    }
+  }
+};
---
+++ app/serializers/i-i-s-disney-ticket-offline.js
@@ -0,0 +1,6 @@
+import { OfflineSerializer as TicketSerializer } from
+  '../mixins/regenerated/serializers/i-i-s-disney-ticket-offline';
+import __ApplicationSerializer from './application-offline';
+
+export default __ApplicationSerializer.extend(TicketSerializer, {
+});
